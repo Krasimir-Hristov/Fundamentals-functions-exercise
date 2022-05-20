a06f595cd6b0e5229ba078c3dfffc1637582a8a6
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-20 17:06:48 +0200

    Add files via upload

---
+++ 02.addAndSubtrack.js
@@ -0,0 +1,16 @@
+function solve(num1,num2,num3){
+
+console.log(sum(num1,num2) - num3)
+
+
+    function sum(a,b){
+        return a + b
+    }
+
+
+}
+solve(23,6,10);
+
+solve(1,17,30);
+
+solve(42,58,100);
