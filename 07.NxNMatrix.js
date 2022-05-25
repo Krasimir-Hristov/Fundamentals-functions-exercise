d014934f10c859fe0650041f559afd5233be526b
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-25 20:26:25 +0200

    Add files via upload

---
+++ 07.NxNMatrix.js
@@ -0,0 +1,14 @@
+function solve(num){
+
+for(let i = 0; i < num; i++){
+   let result = ` `;
+    for(let j = 0; j < num;j++){
+        result += num + ` `;
+       
+    }
+    console.log(result)
+}
+
+
+}
+solve(7)
